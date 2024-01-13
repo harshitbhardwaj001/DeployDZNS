3083fb3f26bd43804c8e4b7e8024a69fe46204f8
Harshit Bhardwaj <68640818+harshitbhardwaj001@users.noreply.github.com>
2024-01-13 17:07:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: process.env.PUBLIC_URL,
+    origin: [process.env.PUBLIC_URL],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
